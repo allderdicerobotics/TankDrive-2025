e1f3d804ce8447948dbf165bc865a51627a70f51
allderdicerobotics <35245530+allderdicerobotics@users.noreply.github.com>
2025-02-09 15:01:56 -0500

    Update robot.py
    added outline to include elevator commands

--- robot.py
+++ robot.py
@@ -29,7 +29,11 @@ class Robot(wpilib.TimedRobot):
                 lambda : self.controller.getRightX()
             )
         )
-        
+
+        #Set default command to update elevator position based on button
+        #Should mirror the above command but with SetElevator
+        #Need to create a variable tracking elevator angle
+        #Update variable according to button commands in periodic below
 
     def teleopPeriodic(self):
         """This function is called periodically during teleoperated mode."""
@@ -39,6 +43,9 @@ class Robot(wpilib.TimedRobot):
         
         if self.run_arm:
             commands2.cmd.RunCommand(lambda : self.arm.outTakePiece(), self.arm)
+
+        #Update the elevator angle variable according to button presses
+        #Can simply be "if self.controller.'button pressed'" then change the value
             
 if __name__ == "__main__":
     wpilib.run(Robot)
